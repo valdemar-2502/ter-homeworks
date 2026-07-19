8bcd1a2b00412cd43f4ecc2287a5334b504b38bd
valdemar-2502 <vs.shah549@gmail.com>
2026-07-19 11:58:34 +0300

    providers.tf

--- 03/src/providers.tf
+++ 03/src/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.130.0" # Актуальная версия провайдера, совместимая с TF 1.12+
+      version = "~> 0.130.0" 
     }
   }
   required_version = "~> 1.12.0"
